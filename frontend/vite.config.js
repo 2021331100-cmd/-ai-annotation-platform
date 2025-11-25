780328bd84759894dcb5bf97db0706d6bf4654b0
2021331100-cmd <2021331100@student.sust.edu>
2025-11-25 13:36:11 -0800

    Add build config to vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false
+  },
   server: {
     port: 3000,
     proxy: {
